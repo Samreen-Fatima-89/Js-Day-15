789bae39e8a047d31935fa0304a846eac04f0363
Samreen Fatima <irfankhans786787789@gmail.com>
2024-09-27 19:07:57 +0500

    completed

---
+++ app.js
@@ -0,0 +1,24 @@
+// var num = parseInt ("234");
+
+// console.log(num);
+
+
+// var num = Number("2.14");
+// console.log(num);
+
+// var num = 3455;
+// var result = num.toString()
+// console.log(num);
+// console.log(result);
+
+// var today = new Date();
+// var str = ["sun","mon","tue","wed","tur","fri","sat"];
+// console.log(str[today.getDay()]);
+
+// var num = 3.9890
+// console.log(Number(num.toFixed(2)));
+
+// var now = new Date();
+//  var theHr = now.getHours();
+// var theMin = now.getMinutes();
+// alert("Current time: "+ theHr + ":" + theMin);
